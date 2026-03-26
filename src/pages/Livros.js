2e1dfcd9237965a41981bc39429f9c3fe1a6e665
vivian garcia <vivian.faculads2024@gmail.com>
2026-03-26 19:46:04 -0300

    Create Livros.js component for managing books

---
+++ src/pages/Livros.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Livros = () => {
+    return (
+        <div>
+            <h1>Gerenciamento de Livros</h1>
+            {/* Como adicionar, remover ou listar livros */}
+        </div>
+    );
+};
+
+export default Livros;
